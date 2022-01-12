19302c01ecce4969332370b603e7ee8a6140c4ff
David Grammatico <david.lugdunum@gmail.com>
2022-01-12 17:34:16 +0100

    add state for multiform

--- src/Components/MultiForm/Multiform.js
+++ src/Components/MultiForm/Multiform.js
@@ -2,6 +2,13 @@ import React, {useState} from 'react'
 import './Multiform.css'
 
 export default function Multiform() {
+  const [formIndex, setFormIndex] = useState(1)
+  const [allFormData, setAllFormData] = useState({
+    dietForm: "",
+    foodStyle: [],
+    allergies: [],
+    prefs: {}
+  })
   return (
     <div>
 
