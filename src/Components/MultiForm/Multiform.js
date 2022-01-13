7b5b43a70f74ceaa99400c497da4d30a9eaf5c64
David Grammatico <david.lugdunum@gmail.com>
2022-01-13 19:52:31 +0100

    add interface for foodStyle

--- src/Components/MultiForm/Multiform.js
+++ src/Components/MultiForm/Multiform.js
@@ -4,6 +4,7 @@ import Indicator from './Indicator/Indicator'
 import CardBegin from './Infos/CardBegin'
 import CardEnd from './Infos/CardEnd'
 import DietForm from './SubForms/DietForm'
+import FoodStyle from './SubForms/FoodStyle'
 
 export default function Multiform() {
   const [formIndex, setFormIndex] = useState(1)
@@ -30,6 +31,7 @@ export default function Multiform() {
       <Indicator />
       { formIndex === 1 ? <CardBegin modifyIndex={modifyIndex} />
       : formIndex === 2 ? <DietForm modifyIndex={modifyIndex} />
+      : formIndex === 3 ? <FoodStyle modifyIndex={modifyIndex}/>
       : ""}
     </div>
   )
--- src/Components/MultiForm/SubForms/FoodStyle.js
+++ src/Components/MultiForm/SubForms/FoodStyle.js
@@ -1,10 +1,39 @@
 import React, {useRef} from 'react'
 import './SubForm.css'
 
-export default function FoodStyle() {
-  return (
-    <div>
+export default function FoodStyle(props) {
 
-    </div>
-  )
+  const preventFunc = e => {
+    e.preventDefault();
+  }
+
+  const handleReturn = () => {
+    props.modifyIndex(2)
+  }
+
+  return (
+    <form onSubmit={preventFunc} className="checkbox-form">
+      <p>Quelles sont tes cuisines préférées ?</p>
+      <span>Choix multiples.</span>
+      <label htmlFor="italian">Italienne</label>
+      <input value="italian" id="italian" type="checkbox" />
+      <label htmlFor="japanese">Japonaise</label>
+      <input value="japanese" id="japanese" type="checkbox" />
+      <label htmlFor="indian">Indienne</label>
+      <input value="indian" id="indian" type="checkbox" />
+      <label htmlFor="thaï">Thaïlandaise</label>
+      <input value="thaï" id="thaï" type="checkbox" />
+      <label htmlFor="french">Française</label>
+      <input value="french" id="french" type="checkbox" />
+      <label htmlFor="chinese">Chinoise</label>
+      <input value="chinese" id="chinese" type="checkbox" />
+      <div className="container-nav-btns">
+        <button
+          onClick={handleReturn}
+          type='button'
+          className='prev'>Précédent</button>
+        <button>Validez</button>
+      </div>
+    </form>
+  );
 }
